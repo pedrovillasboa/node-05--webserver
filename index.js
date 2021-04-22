6f560f05c1393b7c04e7827e4e3cdec782d58056
pedro.villasboa <pedro.villasboa@gmail.com>
2021-04-22 17:28:09 -0300

    primer commit

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express')
 const app = express()
-const port = 4500;
+const port = process.env.PORT;
 
 app.use(express.static(__dirname +'/public'));
  
